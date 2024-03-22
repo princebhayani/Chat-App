d55fce5728d979ce3a3d016e0a5d047402c2a5c7
Prince Bhayani <princebhayani192003@gmail.com>
2024-03-22 13:16:11 +0530

    MongoDB Setup

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,11 +1,14 @@
-export const signup = (req,res)=>{
-    console.log("signup");
-}
+export const signup = (req, res) => {
+  try {
+    const { fullName, username, password, confirmPassword } = req.body;
+  } catch (error) {}
+  console.log("signup");
+};
 
-export const login = (req,res)=>{
-    console.log("login");
-}
+export const login = (req, res) => {
+  console.log("login");
+};
 
-export const logout = (req,res)=>{
-    console.log("logout");
-}
+export const logout = (req, res) => {
+  console.log("logout");
+};
--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,19 @@
 import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
-import authRoutes from "./routes/auth.routes.js"
+import authRoutes from "./routes/auth.routes.js";
+import connectToMongoDB from "./db/connectToMongoDB.js";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.get("/",(req,res)=>{
-    res.send("Hello World!!")
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!!");
+});
 
-app.use('/api/auth',authRoutes);
+app.use("/api/auth", authRoutes);
 
-app.listen(PORT,()=>console.log(`server running on port ${PORT}!!`));
+app.listen(PORT, () => {
+    connectToMongoDB();
+  console.log(`server running on port ${PORT}!!`);
+});
