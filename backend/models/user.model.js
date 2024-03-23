7ec0328df6e88c7e2113a55784d45de5c0dbb5f7
Prince Bhayani <princebhayani192003@gmail.com>
2024-03-23 09:58:29 +0530

    Create User model

---
+++ backend/models/user.model.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    fullName:{
+        type:String,
+        require:true
+    },
+    username:{
+        type:String,
+        require:true,
+        unique:true
+    },
+    password:{
+        type:String,
+        require:true,
+        minlength:6
+    },
+    gender:{
+        type:String,
+        require:true,
+        enum:["male","female"]
+    },
+    profilePic:{
+        type:String,
+        default:""
+    }
+});
+
+const User = mongoose.model("User",userSchema);
+
+export default User;
