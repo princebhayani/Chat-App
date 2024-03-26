83e06edcebca30dfcfe4852f734e76ec45995f68
Prince Bhayani <princebhayani192003@gmail.com>
2024-03-26 23:52:33 +0530

    SignUp Endpoint

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,8 +1,52 @@
-export const signup = (req, res) => {
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+
+export const signup = async(req, res) => {
   try {
-    const { fullName, username, password, confirmPassword } = req.body;
-  } catch (error) {}
-  console.log("signup");
+    const { fullName, username, password, confirmPassword, gender} = req.body;
+    if(password !== confirmPassword){
+      return res.status(400).json({error:"Password don't match"})
+    }
+
+    const user = await User.findOne({username});
+    if(user){
+      return res.status(400).json({error:"Username is already exist"});
+    }
+
+    //hash password
+    const salt = await bcrypt.genSalt(10);
+    const hashPassword = await bcrypt.hash(password,salt);
+
+    //random profile pic
+    const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
+    const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`;
+
+
+    const newUser = new User({
+      fullName,
+      username,
+      password:hashPassword,
+      gender,
+      profilePic: gender==="male"?boyProfilePic :girlProfilePic,
+    });
+
+    if(newUser){
+      //generate JWT token here
+      await newUser.save();
+      res.status(201).json({
+        _id:newUser._id,
+        fullName:newUser.fullName,
+        username:newUser.username,
+        profilePic:newUser.profilePic
+      });
+    }else{
+      res.status(400).json({error:"Invalid user data"});
+    }
+
+  } catch (error) {
+    console.log("Error in signup",error.message);
+    res.status(500).json({error:"Internal Server Error"});
+  }
 };
 
 export const login = (req, res) => {
--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,17 @@
 import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
+
 import authRoutes from "./routes/auth.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
 const app = express();
+app.use(express.json()); // to parse the incoming request with JSON payload(from req.body)
 const PORT = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!!");
-});
+// app.get("/", (req, res) => {
+//   res.send("Hello World!!");
+// });
 
 app.use("/api/auth", authRoutes);
 
