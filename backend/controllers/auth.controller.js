5dabf86c29a454ffc28d48e51f1d66d25c896e0d
Prince Bhayani <princebhayani192003@gmail.com>
2024-03-27 00:12:22 +0530

    generate JWT

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
+import generateTokenAndSetCookie from "../utils/generateToken.js";
 
 export const signup = async(req, res) => {
   try {
@@ -32,6 +33,7 @@ export const signup = async(req, res) => {
 
     if(newUser){
       //generate JWT token here
+      generateTokenAndSetCookie(newUser._id,res);
       await newUser.save();
       res.status(201).json({
         _id:newUser._id,
