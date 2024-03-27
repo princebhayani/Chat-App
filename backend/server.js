b6f4c2eedcebc09e10ef503baf9d35ae229336a4
Prince Bhayani <princebhayani192003@gmail.com>
2024-03-28 00:07:21 +0530

    Get users for sidebar

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,8 @@ import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
+import userRoutes from "./routes/user.routes.js";
+
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
 const app = express();
@@ -19,6 +21,7 @@ app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
+app.use("/api/users", userRoutes);
 
 app.listen(PORT, () => {
     connectToMongoDB();
