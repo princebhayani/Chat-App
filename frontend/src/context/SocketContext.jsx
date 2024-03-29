4021a8762ea0e888d36bde8ac5e1185696794b61
Prince Bhayani <princebhayani192003@gmail.com>
2024-03-29 19:49:36 +0530

    socket url

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({children})=>{
 
     useEffect(()=>{
         if(authUser){
-            const socket = io("http://localhost:8000",{
+            const socket = io("https://chat-app-7l8n.onrender.com/",{
                 query:{
                     userId:authUser._id
                 }
